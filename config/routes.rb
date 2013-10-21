bab5213cf601d6fd25ee9b74dea7d9248101c4d3
Ravi Sinha <ravi.rakesh@gmail.com>
2013-10-21 23:56:26 +0100

    added new page: Team

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root "pages#home" #root_path
   get "about" => "pages#about" #about_path
+  get "team" => "pages#team" #team_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
